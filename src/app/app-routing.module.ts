93dad24f498f28dbf52d8ddfaa08797787fcf41f
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-11-23 18:09:11 -0300

    Configurando rota de acesso a lista de favoritos.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FavoritadosComponent } from './components/favoritados/favoritados.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
     path:'',
     component: HomeComponent
+  },{
+    path:'favoritado',
+    component:FavoritadosComponent
   }
 ];
 
