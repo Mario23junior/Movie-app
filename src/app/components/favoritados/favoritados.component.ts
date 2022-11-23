88c83ab32a9741e1902badf31d9414517cb81d0e
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-11-23 18:20:34 -0300

    Finalizando componentes de listagem de filmes favoritados.
    Componente de listagem de todos os filmes marcado como favoritados

--- src/app/components/favoritados/favoritados.component.ts
+++ src/app/components/favoritados/favoritados.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Movie } from '../model/Movie';
+import { MovieService } from '../service/movie.service';
 
 @Component({
   selector: 'app-favoritados',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./favoritados.component.css']
 })
 export class FavoritadosComponent {
+  
+  list: Movie[] = []
+  listFavorito:Movie[] = []
+ 
+  constructor(private service: MovieService) {
 
+  }
+  ngOnInit(): void {
+    this.findAll()
+ 
+  }
+
+  findAll(): void {
+    this.service.findAll()
+      .subscribe(res => {
+        res.forEach((listFavorito) => {
+          if(listFavorito.favorito){
+            this.listFavorito.push(listFavorito)
+          }
+        })
+        this.list = res;
+      })
+  }
+
+  favoritar(movie: Movie) {
+    this.service.favoritar(movie)
+      .subscribe(resfavor => {
+        movie.favorito = !movie.favorito
+      })
+  }
 }
